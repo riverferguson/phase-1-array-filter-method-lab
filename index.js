94b10cbf2b8a6071b966582b7b99b3b07d474b0c
River Ferguson <riverferguson@gmail.com>
2023-04-14 18:13:33 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, searched) {
+    return drivers.filter(
+      (driver) => driver.toLowerCase() === searched.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(drivers, searched) {
+    return drivers.filter(
+        (driver) => Array.from(driver.toLowerCase())[0] === Array.from(searched.toLowerCase())[0]
+    )
+
+  }
+
+  function matchName(drivers, searched) {
+    return drivers.filter((driver) => driver.name === searched)
+  }
